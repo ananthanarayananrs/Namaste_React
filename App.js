0a1a96cc0966ae5c01591da06e4de8e8c3122777
Anantha Narayanan R S <ananthanarayananrs@gmail.com>
2023-01-04 18:47:42 +0530

    react part 2 done

---
+++ App.js
@@ -0,0 +1,4 @@
+const heading = React.createElement('h1',{},"Namaste Everyone from react");
+const heading2 = React.createElement('h2',{},"Namaste from raect");
+const root = ReactDOM.createRoot(document.getElementById('root'));
+root.render([heading, heading2]);
